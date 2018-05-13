f108b48494c3c840a35ceabc81957cdf09a4f571
sastolarski <sastolarski@gmail.com>
2018-05-13 13:15:09 -0500

    databas works

--- assets/logic/javascript.js
+++ assets/logic/javascript.js
@@ -10,3 +10,45 @@
   firebase.initializeApp(config);
 
   var database = firebase.database();
+  $("#submit").on("click", function(event){
+
+    event.preventDefault();
+
+      var name = $("#trainName").val().trim();
+      var destination = $("#destination").val().trim();
+      var firstTrain = $("#firstTrain").val().trim();
+      var frequency = $("#frequency").val().trim();
+
+      database.ref().push({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+      });
+
+  })
+
+  database.ref().on("child_added", function(snap){
+      console.log(snap.val());
+
+      $("#tableInfo").append(
+        "<tr>"
+        + "<td>"
+        + snap.val().name
+        + "</td>"
+        + "<td>"
+        + snap.val().destination
+        + "</td>"
+        + "<td>"
+        + snap.val().frequency
+        + "</td>"
+        + "<td>"
+        + "next arrival"
+        + "</td>"
+        + "<td>"
+        + "minutes away"
+        + "</td>"
+        + "</tr>"
+    )
+
+  })
