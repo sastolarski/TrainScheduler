22e73362b8a1f1d8001fa068983ab516fabf812b
sastolarski <sastolarski@gmail.com>
2018-05-13 12:25:57 -0500

    added firebase and jquery

---
+++ assets/logic/javascript.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAL4e_XXLvD_2KivvW0GvAzxIY8rsWwUxc",
+    authDomain: "trainschedule-fe9da.firebaseapp.com",
+    databaseURL: "https://trainschedule-fe9da.firebaseio.com",
+    projectId: "trainschedule-fe9da",
+    storageBucket: "",
+    messagingSenderId: "1076729219884"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
